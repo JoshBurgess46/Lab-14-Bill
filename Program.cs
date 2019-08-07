7165721fc608b9feb172fac21e7a4e6a2e0e1fe7
Josh Burgess <JoshBurgess_46@outlook.com>
2019-08-07 08:53:21 -0400

    Lab 14 Bill

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab_14_Bill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bill billing = new Bill();
+            billing.TaxRate = 0.06;
+            billing.Subtotal = 15.80;
+            Pay(billing);
+
+            
+            TippableBill tip = new TippableBill();
+            tip.Subtotal = 8.50;
+            tip.TaxRate = 0.06;
+            tip.Tip = 2.00;
+            Pay(tip);
+
+        }
+        public static void Pay(Bill bill)
+        {
+            Console.WriteLine($"${bill.CalcTotal()}");
+        }
+    }
+}
---
+++ TippableBill.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab_14_Bill
+{
+    class TippableBill:Bill
+    {
+        public double Tip { get; set; }
+
+        public TippableBill()
+        {
+
+        }
+        public TippableBill(double tip, double subtotal, double taxRate) :base(subtotal,taxRate)
+        {
+            Subtotal = subtotal;
+            TaxRate = taxRate;
+            Tip = tip;
+        }
+        public override double CalcTotal()
+        {
+            return base.CalcTotal() + Tip;//base calls on the parent class method 
+        }
+    }
+}
